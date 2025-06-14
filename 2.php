4b5a722453a2d6fe603a61377b33a1784ffff6b1
Tnmgdrelifousaem <99417161+Tnmgdrelifousaem@users.noreply.github.com>
2025-06-14 18:20:09 +0300

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,14 @@
+function mathOperation($arg1, $arg2, $operation) {
+    switch ($operation) {
+        case 'add':
+            return add($arg1, $arg2);
+        case 'subtract':
+            return subtract($arg1, $arg2);
+        case 'multiply':
+            return multiply($arg1, $arg2);
+        case 'divide':
+            return divide($arg1, $arg2);
+        default:
+            return "Неизвестная операция";
+    }
+}
